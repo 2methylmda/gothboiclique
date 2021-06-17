78277f3edc320b7fdcf0f72513e0857cbf125b6c
Amy <85847631+AmyEatsMDA@users.noreply.github.com>
2021-06-17 17:28:30 -0400

    Create currencies.cpp

---
+++ currencies.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+  
+    double pesos;
+    double reais;
+    double soles;
+    double dollars;
+
+      std::cout << "Enter number of Colombian Pesos: ";
+      std::cin >> pesos;
+      std::cout << "Enter number of Brazilian Reals: ";
+      std::cin >> reais;
+      std::cout << "Enter number of Spanish Sols: ";
+      std::cin >> soles;
+
+      // Pesos conversion rate: 0.00032
+      // Reais conversion rate: 0.27
+      // Soles conversion rate: 0.3
+
+    dollars = 0.00032 * pesos + 0.27 * reais + 0.3 * soles;
+        std::cout << "US Dollars = $" << dollars << "\n";
+}
